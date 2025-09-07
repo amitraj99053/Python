9ca22b309fd8ef8a9e0714a3c5fc018c17e49d6e
Amit Kumar <amitraj99053@gmail.com>
2025-09-07 23:51:24 +0530

    Upload

---
+++ stringFormat.py
@@ -0,0 +1,30 @@
+age = 21
+txt = f"My name is Amit, I am {age} young"
+print(txt)
+
+price = 59
+txt1 = f"The price is {price:.2f} dollars"
+print(txt1)
+
+
+txt2 = f"The price is {20 * 5} dollars"
+print(txt2)
+
+
+txt3 = "We are the so-colled \"Vikinhs\" from the north"
+print(txt3)
+
+txt4 = 'It\'s alright.'
+print(txt4) 
+
+
+txt5 = "This will insert one \\ (backslash)."
+print(txt5) 
+
+
+txt6 = "Hello\nWorld!"
+print(txt6)
+
+ 
+txt7 = "Hello\rWorld!"
+print(txt7) 
