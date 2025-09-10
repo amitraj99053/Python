944dae74bad4f04b37e7696eb0eabee94ae62e32
Amit Kumar <amitraj99053@gmail.com>
2025-09-11 01:04:15 +0530

    Upload

---
+++ tuple.py
@@ -0,0 +1,20 @@
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+print(len(thistuple))
+
+# One item tuple, remember the comma
+thistuple1 = ("apple",)
+print(type(thistuple1))
+
+#NOT a tuple
+thistuple1 = ("apple")
+print(type(thistuple1))
+
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
