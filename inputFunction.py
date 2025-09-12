0e8bb9b21099c3f81573dd1fc50b6e846e6f224f
Amit Kumar <amitraj99053@gmail.com>
2025-09-12 23:43:18 +0530

    Upload

---
+++ inputFunction.py
@@ -0,0 +1,23 @@
+# print("Enter your name:")
+# x = input()
+# print("Hello, " + x)
+
+
+# print("Enter your name:")
+# name = input()
+# print(f"Hello {name}")
+
+
+# name = input("Enter your name:")
+# print(f"Hello {name}")
+
+
+import math
+
+
+x = input("Enter a number:")
+
+#find the square root of the number:
+y = math.sqrt(float(x))
+
+print(f"The square root of {x} is {y}")
