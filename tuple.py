b5b007e09c07b3d2def911cfa38fe00d0372b724
Amit Kumar <amitraj99053@gmail.com>
2025-09-13 00:02:05 +0530

    Upload

--- tuple.py
+++ tuple.py
@@ -18,3 +18,16 @@ tuple3 = (True, False, False)
 print(tuple1)
 print(tuple2)
 print(tuple3)
+
+
+tuple4 = ("abc", 34, True, 40, "male")
+print(tuple4)
+print(tuple4[1])  
+print(tuple4[-1])   # Negative Indexing
+print(tuple4[2:5])  # Range of Indexes
+print(tuple4[-4:-1])  # Range of Negative Indexes
+
+
+tuple4 = ("apple", "banana", "cherry")
+if "apple" in tuple4:
+  print("Yes, 'apple' is in the fruits tuple")
