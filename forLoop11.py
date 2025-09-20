ea0ac04923a78a5763c65f1d67663a1955886280
Amit Kumar <amitraj99053@gmail.com>
2025-09-21 00:18:31 +0530

    Upload

---
+++ forLoop11.py
@@ -0,0 +1,9 @@
+# Print each adjective for every fruit
+# Nested Loops
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
