fff60aa705fa65aba980a5988c4c1a631321fb46
Amit Kumar <amitraj99053@gmail.com>
2025-09-28 15:54:06 +0530

    Upload

---
+++ Sets/sets1.py
@@ -0,0 +1,12 @@
+# Create a Set
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+print(type(thisset))
+# Output: {'banana', 'cherry', 'apple'}
+
+print()
+
+
+# Duplicate values will be ignored
+thisset1 = {"apple", "banana", "cherry", "apple"}
+print(thisset1)
