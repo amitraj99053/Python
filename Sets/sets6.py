12642f23339921acdc9f0726bc432ecdb77194dc
Amit Kumar <amitraj99053@gmail.com>
2025-09-30 21:03:25 +0530

    Upload

---
+++ Sets/sets6.py
@@ -0,0 +1,8 @@
+# Add an item to a set, using the add() method
+
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+thisset.add("Mango")
+print(thisset)
---
+++ Sets/sets7.py
@@ -0,0 +1,6 @@
+# Add elements from tropical into thisset
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+print(thisset)
