87c6a6e26293114218cada0d009810b6f5fe52c7
Amit Kumar <amitraj99053@gmail.com>
2025-10-08 22:29:03 +0530

    Upload

---
+++ Sets/sets33.py
@@ -0,0 +1,9 @@
+# Add an element to the fruits set
+# add() method
+
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+print(fruits)
+
+# Note: If the element already exists, the add() method does not add the element.
