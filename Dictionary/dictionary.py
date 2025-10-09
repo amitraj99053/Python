13a9d81e96c0e8dc49ad58c6516020312ef3789a
Amit Kumar <amitraj99053@gmail.com>
2025-10-09 23:46:14 +0530

    Upload

---
+++ Dictionary/dictionary.py
@@ -0,0 +1,28 @@
+# Create and print a dictionary
+# Duplicate keys are not allowed
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "colors": ["red", "white", "blue"]
+}
+
+my_car = {
+    "brand": "Toyota",
+    "model": "Corolla",
+    "year": 2020
+}
+
+
+print(thisdict)
+print(thisdict["model"])
+print(len(thisdict))
+print(type(thisdict))
+print() # thsis is just for a new line
+
+
+print(my_car)
+print(my_car["brand"])
+print(len(my_car))
+print(type(my_car))
---
+++ Dictionary/dictionary1.py
@@ -0,0 +1,7 @@
+# The dict() constructor
+my_dict = dict(name="Amit", age=30, city="New York")
+
+print(my_dict)
+print(my_dict["name"])
+print(len(my_dict))
+print(type(my_dict))
