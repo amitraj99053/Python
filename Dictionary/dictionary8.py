e7cc58eeaa2d6b285edd98c0738d8c9b7c9f6642
Amit Kumar <amitraj99053@gmail.com>
2025-10-19 21:37:47 +0530

    Upload

---
+++ Dictionary/dictionary8.py
@@ -0,0 +1,11 @@
+# Add dictionary items
+
+my_dict = {
+    'name': 'Alice',
+    'age': 30,
+    'city': 'New York',
+    'occupation': 'Engineer'
+}
+
+my_dict['hobby'] = 'Photography'
+print(my_dict)
---
+++ Dictionary/dictionary9.py
@@ -0,0 +1,14 @@
+# update dictionary
+
+my_dict = {
+    'name': 'Alice',
+    'age': 30,
+    'city': 'New York',
+}
+
+my_dict.update({'occupation': 'Engineer'})
+print(my_dict)
+print()
+
+my_dict.update({'age': 31, 'city': 'San Francisco'})
+print(my_dict)
