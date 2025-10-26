1a473c8edf4f006e4653ef3728b318b70e56a987
Amit Kumar <amitraj99053@gmail.com>
2025-10-26 18:47:40 +0530

    Upload

---
+++ Operators/membership.py
@@ -0,0 +1,13 @@
+# Membership Operators
+
+x = ["apple", "banana", "cherry"]
+print("banana" in x)
+print("grape" not in x)
+print("banana" not in x)
+print()
+
+text = "Hello World"
+
+print("H" in text)
+print("hello" in text)
+print("world" not in text)
