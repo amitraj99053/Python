33345312ab98f34947a1ac36b2e9e434bcabee4c
Amit Kumar <amitraj99053@gmail.com>
2025-10-27 21:27:05 +0530

    Update

---
+++ Operators/bitwise.py
@@ -0,0 +1,13 @@
+# Bitwise operator are used to compaire binary numbers. 
+
+a = 6           # 6 = 0110
+b = 3           # 3 = 0011
+
+print("a & b =", a & b)   # AND Operator:  0110 & 0011 = 0010 = 2
+print("a | b =", a | b)   # OR Operator:   0110 | 0011 = 0111 = 7
+print("a ^ b =", a ^ b)   # XOR Operator:  0110 ^ 0011 = 0101 = 5
+print("~a =", ~a)         # NOT Operator:  ~0110 = 1001 = -7
+print("a << 1 =", a << 1) # Left Shift:    0110 << 1 = 1100 = 12
+print("a >> 1 =", a >> 1) # Right Shift:   0110 >> 1 = 0011 = 3         
+print("b << 2 =", b << 2) # Left Shift:    0011 << 2 = 1100 = 12
+print("b >> 1 =", b >> 1) # Right Shift:   0011 >> 1 = 0001 = 1
