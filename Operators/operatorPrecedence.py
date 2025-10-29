e46cb0605bec11ce8bc0863334835fd66deb4089
Amit Kumar <amitraj99053@gmail.com>
2025-10-29 23:07:03 +0530

    Upload

---
+++ Operators/operatorPrecedence.py
@@ -0,0 +1,32 @@
+print((6 + 3) - (6 + 3)) # parentheses
+
+print(100 + 5 * 3) # multiplication
+
+print(100 - 3 ** 3) # exponentiation
+
+print(100 + -3) # unary negation
+
+print(100 + +3) # unary plus
+
+print(-100) # bitwise not
+
+print(100 / 5) # division
+
+print(100 // 6) # floor division
+
+print(100 % 6) # modulus
+
+print(100 + 5) # addition
+
+print(100 - 5) # subtraction
+
+print(2 << 2) # left shift
+
+print(8 >> 2) # right shift
+
+print(6 & 3) # bitwise AND
+
+print(6 | 3) # bitwise OR
+
+print(6 ^ 3) # bitwise XOR
+
