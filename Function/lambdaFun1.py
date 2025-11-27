c5622b1aa1acc7a39c09c664756e65276b0dd810
Amit Kumar <amitraj99053@gmail.com>
2025-11-27 21:32:56 +0530

    Upload

---
+++ Function/lambdaFun1.py
@@ -0,0 +1,15 @@
+# Add 10 to argument a, and return the result:
+
+x = lambda a : a + 10
+print(x(5))
+
+# Multiply argument a with argument b and return the result
+
+y = lambda c, d : c * d
+print(y(5, 6))
+
+# Summarize argument a, b, and c and return the result
+
+z = lambda e, f, g : e + f + g
+print(z(5, 6, 2))
+
