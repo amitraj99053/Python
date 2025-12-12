d74a0fd7830627bc07da0aaffd75f29b4d041a33
Amit Kumar <amitraj99053@gmail.com>
2025-12-12 21:39:17 +0530

    Upload

---
+++ Range/range7.py
@@ -0,0 +1,5 @@
+# Test if the numbers 6 and 7 are present in a range
+
+r = range(0, 10, 2)
+print(6 in r)
+print(7 in r)
---
+++ Range/range8.py
@@ -0,0 +1,3 @@
+# Get the length of a range
+r = range(0, 10, 2)
+print(len(r))
