14c6c63fd94bfb8a2e235eee850df94997e670b5
Amit Kumar <amitraj99053@gmail.com>
2025-12-13 23:30:33 +0530

    Upload Iterators examples

---
+++ Iterators/iterator1.py
@@ -0,0 +1,8 @@
+# Return an iterator from a tuple, and print each value
+
+mytumple = ("apple", "banana", "cherry")
+myit = iter(mytumple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
---
+++ Iterators/iterator4.py
@@ -0,0 +1,6 @@
+# Iterate the characters of a string
+
+mystr = "banana"
+
+for x in mystr:
+    print(x)
