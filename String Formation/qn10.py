bf9ee99d8a75cd5b6faaf4f87a7968189fc6378c
Amit Kumar <amitraj99053@gmail.com>
2025-12-29 18:06:32 +0530

    Upload

---
+++ String Formation/qn10.py
@@ -0,0 +1,5 @@
+# Use a comma as a thousand separator
+
+price = 59000
+txt = f"The price is {price:,} dollars"
+print(txt)
---
+++ String Formation/qn8.py
@@ -0,0 +1,5 @@
+# Use the string method upper() to convert a value into upper case letters
+
+fruit = "apples"
+upper_fruit = f"I love {fruit.upper()}"
+print(upper_fruit)
