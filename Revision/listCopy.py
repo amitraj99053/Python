f48f72dbfca6cb157bca27259b0128d2aeff6450
Amit Kumar <amitraj99053@gmail.com>
2026-01-20 23:11:25 +0530

    Python - Copy Lists

---
+++ Revision/listCopy.py
@@ -0,0 +1,16 @@
+# Make a copy of a list with the copy() method
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+print()
+
+
+# Make a copy of a list with the list() method
+mylist2 = list(thislist)
+print(mylist2)
+print()
+
+
+# Make a copy of a list with the : operator
+mylist3 = thislist[:]
+print(mylist3)
